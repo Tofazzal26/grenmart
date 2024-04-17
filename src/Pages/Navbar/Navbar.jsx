dc9de6dfc6276c2489b5e7a2fdfa73b44e7d1569
Tofazzal26 <afranislamabir6789@gmail.com>
2024-04-17 11:35:03 +0600

    navbar style finish

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,7 +1,81 @@
+import { IoMdMenu } from "react-icons/io";
+import { BiDownArrow } from "react-icons/bi";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <div className="grid grid-cols-4">
+        <div className="col-span-1">
+          <details className="dropdown">
+            <summary className="bg-[#00b106] flex items-center justify-between text-lg cursor-pointer font-semibold gap-[50px] px-12 text-white py-3 rounded-full">
+              <IoMdMenu size={22} /> All Categories <BiDownArrow size={21} />
+            </summary>
+            <ul className="p-2 shadow menu dropdown-content space-y-2 z-[1] text-[#767676] w-[350px] text-[17px] font-Source3 rounded-box bg-[#f8f9fa]">
+              <li className="border-b">
+                <a className="p-3">Home</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3 flex justify-between items-center">
+                  Fruits & Vegetables{" "}
+                  <MdKeyboardArrowRight size={25} color="#999999" />
+                </a>
+              </li>
+              <li className="border-b">
+                <a className="p-3 flex justify-between items-center">
+                  Cooking <MdKeyboardArrowRight size={25} color="#999999" />
+                </a>
+              </li>
+              <li className="border-b">
+                <a className="p-3 flex justify-between items-center">
+                  Meat & Fish <MdKeyboardArrowRight size={25} color="#999999" />
+                </a>
+              </li>
+              <li className="border-b">
+                <a className="p-3">Cooking</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3">Baking</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3"> Dairy</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3">Candy & Chocolate</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3">Frozen & Canned</a>
+              </li>
+              <li className="border-b">
+                <a className="p-3">Bread & Bakery</a>
+              </li>
+              <li>
+                <a className="p-3">Snacks</a>
+              </li>
+            </ul>
+          </details>
+        </div>
+        <div className="col-span-3">
+          <div className="bg-[#f8f9fa] rounded-full p-4">
+            <ul className="flex items-center font-Source3 px-2 font-semibold text-sm gap-8">
+              <li className="flex items-center gap-2 cursor-pointer">
+                Home Page <MdKeyboardArrowDown color="#999999" size={20} />
+              </li>
+              <li className="cursor-pointer">About</li>
+              <li className="flex items-center cursor-pointer gap-2">
+                Shop Page <MdKeyboardArrowDown color="#999999" size={20} />
+              </li>
+              <li className="flex items-center cursor-pointer gap-2">
+                Pages <MdKeyboardArrowDown color="#999999" size={20} />
+              </li>
+              <li className="flex items-center cursor-pointer gap-2">
+                Blog <MdKeyboardArrowDown color="#999999" size={20} />
+              </li>
+              <li className="cursor-pointer">Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
   return (
     <div className="container mx-auto">
       <Header />
+      <Navbar />
       <Outlet />
       <h1>Footer</h1>
     </div>
